6729c2b397d42e685b961d1c473a5df038e9e16d
карина <@DESKTOP-FP9I0KA>
2024-09-26 14:46:22 +0300

    Add project files.

---
+++ Raindrops/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Raindrops
+{
+    internal class Program
+    {
+        public static string ConvertToRainSound(int number)
+        {
+            string result = "";
+
+           
+            if (number % 3 == 0)
+            {
+                result += "Pling";
+            }
+            if (number % 5 == 0)
+            {
+                result += "Plang";
+            }
+            if (number % 7 == 0)
+            {
+                result += "Plong";
+            }
+            return string.IsNullOrEmpty(result) ? number.ToString() : result;
+        }
+
+        static void Main()
+        {
+            
+            int[] numbersToCheck = { 28, 30, 34 };
+
+            foreach (int number in numbersToCheck)
+            {
+                string result = ConvertToRainSound(number);
+                Console.WriteLine($"{number}: {result}");
+            }
+        }
+    }
+}
